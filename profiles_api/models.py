73a84ed439f223160ba872f2627838eecb5c7b01
Li Zhu <juliezhuli@outlook.com>
2022-07-22 14:46:04 -0700

    add custom user profile model, manager and migration

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Manager for the user profiles"""
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('users must have an email')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name,) #create a new model that the user manager is representing
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    #need to specify the model manager that we're going to use for the objects and this is required because we need to use our custom user model with the Django CLI so Django needs to have a custom model manager for the user model so it knows how to create and control users
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Retrieve full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """Retrieve short name of user"""
+        return self.name
+
+    #make string representation to be a meaningful output, for reading in django admin or when printing code
+    def __str__(self):
+        """Return string rep of our user"""
+        return self.email
