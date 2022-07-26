08b64d89a7255c278ecea8c40cf794e81fdfbea8
Li Zhu <juliezhuli@outlook.com>
2022-07-26 14:18:43 -0700

    add login api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -11,6 +11,7 @@ router.register('profile', views.UserProfileViewSet) #don't need to specify base
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()), #call to convert our api view class to be rendered by our urls
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls)),
     #as you register new routes with our router it generates a list of URLs
     #that are associated for our view set it figures out the URLs that are required
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers
 from profiles_api import models
@@ -124,3 +126,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+class UserLoginApiView(ObtainAuthToken):
+   """Handle creating user authentication tokens"""
+   #enable it in the browsable Django admin site so we need to override this class
+   #and customize it so it's visible in the browsable api for test
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
